968f310a74a4feed2e18896c5220e75467d6f84e
unknown <dinggididududu.com>
2021-07-13 00:41:43 -0400

    second commit. find out prefix

--- CST8208_21S_BA2/src/CST8208_21S_BA2.java
+++ CST8208_21S_BA2/src/CST8208_21S_BA2.java
@@ -37,18 +37,23 @@ import java.util.regex.Pattern;
 
 public class CST8208_21S_BA2 {
 	public static void main(String[] args) {
-		String address;
-		String subnet;
+		String ip_string;
+		char ipClass;//A or B or C
 		
-		String addressip;
-		String addressBinary;
+		String[] subnet;
+		String[] ipArr = new String[5];
+		
+//		String addressip;
+		String ipBinary;
+		String subnetBinary;
 		String binaryFormat;
 //		String a,b,c,d,e;
 //		int ii;
-		byte[] bytes;
-		
+		byte[] bytes1;
+		byte[] bytes2;
+//		
 		DecimalFormat formatter = new DecimalFormat("########,########,########,########");
-		String ipFormat = formatter.toString().replace(",", ".");
+//		String ipFormat = formatter.toString().replace(",", ".");
 //		a="^([1-255])$"+ ".";
 //		b="^([1-255])$"+ ".";
 //		c="^([1-255])$"+ ".";
@@ -58,41 +63,42 @@ public class CST8208_21S_BA2 {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.printf("Input: ");
-		address = sc.next();
-		subnet = sc.next();
-//		addBinary = 
-	
-
-//		i = Integer.parseInt(address, 10);
+		ip_string = sc.nextLine();
+		
+		String ip = ip_string.substring(ip_string.lastIndexOf("/"));
+		ipArr = ip.split("[.]");
+		subnet = ip_string.split("/"); //subnet=subnet[1]
+//		System.out.println(ipArr.length);
+//		for(int i=0;i<ipArr.length;i++) {
+//			System.out.println(ipArr[i]);
+//		} 
+		sc.close();
 		try {
-			
-			
-			addressip = "^([01]?\\d|2[0-4]\\d|25[0-5])\\." + "^([01]?\\d|2[0-4]\\d|25[0-5])\\."
-						+ "^([01]?\\d|2[0-4]\\d|25[0-5])\\." + "^([01]?\\d|2[0-4]\\d|25[0-5])\\.";
-//			addBinary = "^\d";
-			bytes = InetAddress.getByName(address).getAddress();
-			addressBinary = new BigInteger(1, bytes).toString(2);
-			binaryFormat = formatter.format(Double.parseDouble(addressBinary)).toString().replace(",", ".");
+			bytes1 = InetAddress.getByName(ip).getAddress();
+			bytes2 = InetAddress.getByName(subnet[1]).getAddress();
+//			bytes2 = InetAddress.getByName(subnet[1]).getAddress();
+			ipBinary = new BigInteger(1, bytes1).toString(2);
+			subnetBinary = new BigInteger(1, bytes2).toString(2);
+//			subnetBinary = Integer.toHexString(Integer.parseInt(subnet[1]));//print 1 in subnet[1] times //change prefix to binary
 			
 			System.out.println("Output :");
-		
-					//calculate 
-			System.out.printf("Address :" + "%15s" + "%40s\n", address, binaryFormat);
-			System.out.printf("Netmask: " + "%25s = %s" + "%33s\n", subnet);
-//			System.out.println("Wildcard: " + "%24f" + "%33f");
-//			System.out.println("=>\nSubnet (Network):" + "%17f" + "%33f");
-//			System.out.println("Broadcast: " + "%23f" + "%33f");
-//			System.out.println("HostMin (FHIP): " + "%19f" + "%33f");
-//			System.out.println("HostMax (LHIP): " + "%19f" + "%33f");
+//		
+//					//calculate 
+			System.out.printf("Address :" + "%15s" + "%40s\n", ip , formatter.format(Double.parseDouble(ipBinary)).toString().replace(",","."));
+			System.out.printf("Netmask: " + "%15s = %s" + "%40s\n",  , subnet[1], formatter.format(Double.parseDouble(subnetBinary)).toString().replace(",","."));
+//			System.out.println("Wildcard: " + "%20s" + "%40s");
+//			System.out.println("=>\nSubnet (Network):" + "%17s" + "%40s" + "Class %c", , , ipClass);
+//			System.out.println("Broadcast: " + "%23f" + "%40s");
+//			System.out.println("HostMin (FHIP): " + "%19f" + "%40s");
+//			System.out.println("HostMax (LHIP): " + "%19f" + "%40s");
 //			System.out.println("s=" +);
 //			System.out.println("S=" +);
 //			System.out.println("Subnet Index (%d) = " +);
 //			System.out.println("h=" + );
 //			System.out.println("HIPs Hosts/Net: " + );
 		
-		} catch (UnknownHostException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		sc.close();
 	}
 }
