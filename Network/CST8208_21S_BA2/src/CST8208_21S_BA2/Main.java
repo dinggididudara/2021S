a8ff6c88ed2e11264e586287cc237e84a089eb35
dinggididudara <dinggididududu@gmail.com>
2021-08-01 17:10:40 -0400

    added comments

--- Network/CST8208_21S_BA2/src/CST8208_21S_BA2/Main.java
+++ Network/CST8208_21S_BA2/src/CST8208_21S_BA2/Main.java
@@ -1,94 +1,67 @@
-package CST8208_21S_BA2;
-/*
-Output:
-Address:   32.20.130.0           00100000.00010100.1 0000010.00000000
-Netmask:   255.255.128.0 = 17    11111111.11111111.1 0000000.00000000
-Wildcard:  0.0.127.255           00000000.00000000.0 1111111.11111111
-=>
-Sunnet (Network):32.20.128.0/17  00100000.00010100.1 0000000.00000000 (Class A)
-Broadcast:       32.20.255.255   00100000.00010100.1 1111111.11111111
-HostMin (FHIP):  32.20.128.1     00100000.00010100.1 0000000.00000001
-HostMax (LHIP):  32.20.255.254   00100000.00010100.1 1111111.11111110
-s=9
-S=512
-Subnet Index (000101001) = 41
-h=15
-HIPs Hosts/Net: 32766  
-
-Your code should be uploaded to Brightspace (via the link posted) in a single zip file obtained by:
-1) In Eclipse, selecting the project name
-(CST8208_20S_BA2)
-2) right clicking on ‘Export’ and selecting General/Archive File; click Next;
-3) in the Archive File menu make sure all of the project subfolders are selected (src, bin, .settings)
-and the ‘Save in zip format’ and ‘Create in Directory Structure’ radio buttons are selected
-4) In the ‘To Archive File’ window, save your zip file to a location you’ll remember. But make certain
-the name of your zip file corresponds to the following format: BA_Yourlastname_Yourfirstname.zip
-including the underscores and capitals, but with your last and first name inserted as indicated, and
-where your last name and first name are the same is used in Brightspace.
-
+/**
+ * @author Soomin L
+ * {@summary CST8208_21S_BA2: subnetting program. Any variables named with 'Full' is the result for printing.}
+ * @date 01/Aug/2021
  */
+package CST8208_21S_BA2;
 
-import java.math.BigInteger;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.text.DecimalFormat;
-import java.util.Arrays;
 import java.util.Scanner;
-import java.util.regex.Pattern;
 import org.apache.commons.net.util.SubnetUtils;
-
 
 public class Main {
 	static int s; //subnet bits
 	static int h; //how many bits borrowed //host bits
-	static int host;
-	static char ipClass;
-	static int subnetPrefix;
-	static String networkIp;
-	static String broadcastIp;
-	static String[] ipArr = new String[4];
-	static int[] ipArrInt = new int[4];
+	
+	static int subnetPrefix; //subnet prefix
+	
+	static String[] ipArr = new String[4]; //array for ip (String)
+	static int[] ipArrInt = new int[4];  //array for ip (int)
+	
 	public static void main(String[] args) {
 		String ip_string;
 		char ipClass = 0;//A or B or C
-		
-		String[] subnet;
 	
 		int subnetIndex;
 		int subnetIndexBinary;
-		
-		String ipBinary;
-		String binaryFormat;
-		
+	
+//		char ipClass;
+		String networkIp;
+		String broadcastIp;
 		String ipBinaryFull;
 		
-		String networkIpBinary;
+		String networkIpBinaryFull;
 		String[] networkIpArr;
 		
-		String broadcastIpBinary;
+		String broadcastIpBinaryFull;
 		String[] broadcastIpArr;
 		
 		String hostMinIp;
 		String[] hostMinIpArr;
-		String hostMinBinary;
+		String hostMinBinaryFull;
 		
-		String hostMaxBinary;
+		String hostMaxBinaryFull;
 		String hostMaxIp;
 		String[] hostMaxIpArr;
 		
-//------------start here---------------------------------------------------------------------------------------------------------------------//	
+//-----------------------------------<start here>-------------------------------------------------------------------------------------------------------------------------------------------------------//	
 		
-		Scanner sc = new Scanner(System.in); //get input from user
-		System.out.printf("Input: ");
-		ip_string = sc.nextLine();
 		
-		String ip = ip_string.substring(0,ip_string.indexOf("/")); //input ip except prefix
+//--------1.get user's input---------------------------------------------------------------------------------------------------------------------------------------------------//		
+		Scanner sc = new Scanner(System.in); //get input from user																											   //
+		System.out.printf("Input: ");																																		   //
+		ip_string = sc.nextLine();																																			   //
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------//		
+		/**
+		 * analyze user input ip
+		 */
+		String ip = ip_string.substring(0,ip_string.indexOf("/")); //input ip except prefix (String)
 		ipArr = ip.split("[.]"); //split to 4 parts and save into array (String)
 		for(int i=0;i<4;i++) { //ipArray: change to integer type (int)
 			ipArrInt[i] = Integer.parseInt(ipArr[i]);
 		}
-		subnetPrefix = Integer.parseInt(ip_string.substring(ip_string.indexOf("/")+1,ip_string.length())); //subnet to integer type
+		subnetPrefix = Integer.parseInt(ip_string.substring(ip_string.indexOf("/")+1,ip_string.length())); //subnet to integer type (int)
 		
+		//calculate class with subnet bits and host bits
 		if(ipArrInt[0] <= 127) {
 			ipClass = 'A';
 			s =  subnetPrefix - 8; //(subnet prefix) minus (class A's prefix) //2^s = total subnets
@@ -102,8 +75,11 @@ public class Main {
 			s = subnetPrefix -24;
 			h = 8 - s;
 		}
-		
+		/**
+		 * calculate 
+		 */
 		try {
+			
 			SubnetUtils subnetUtils = new SubnetUtils(ip_string);
 			subnetUtils.setInclusiveHostCount(true);
 			
@@ -111,48 +87,76 @@ public class Main {
 			
 			SubnetMask subnetMask = new SubnetMask();
 			subnetMask.subnetMaskBinary(subnetPrefix);
+//----------------2.print address, subnetmask, wildcard----------------------------------------------------------------------------------------------//	
+			System.out.println("\nOutput:");																										 //
+			System.out.printf("Address: %17s %47s\n", ip , ipBinaryFull);                                                                    		 //
+			System.out.printf("Netmask: %17s = %s %42s\n", SubnetMask.subnetIpString, subnetPrefix, SubnetMask.subnetMaskBinaryFull);                 //
+			System.out.printf("Wildcard: %16s %47s\n", SubnetMask.wildcardStrFull, subnetMask.wildcardBinStrFull);														 //			
+			System.out.println("=>");																												 //	
+//---------------------------------------------------------------------------------------------------------------------------------------------------//	
 			
-			System.out.println("\nOutput:");
-			System.out.printf("Address: %17s %47s\n", ip , ipBinaryFull);
-			System.out.printf("Netmask: %17s = %s %42s\n", SubnetMask.subnetDecimalString, subnetPrefix, SubnetMask.subnetMaskFull);
-			System.out.printf("Wildcard: %16s %47s\n", SubnetMask.wildcardStr, subnetMask.d);
-			System.out.println("=>");
 			
+			
+			
+			
+			
+			/**
+			 * calculating network ip and broadcast ip
+			 */
 			networkIp = subnetUtils.getInfo().getNetworkAddress();
 			networkIpArr = networkIp.split("[.]");
 			int[] networkIpArrInt = { Integer.parseInt(networkIpArr[0]),Integer.parseInt(networkIpArr[1]),Integer.parseInt(networkIpArr[2]),Integer.parseInt(networkIpArr[3])};
-			networkIpBinary = String.format("%08d.%08d.%08d.%08d", Integer.parseInt(Integer.toBinaryString(networkIpArrInt[0])),Integer.parseInt(Integer.toBinaryString(networkIpArrInt[1])),Integer.parseInt(Integer.toBinaryString(networkIpArrInt[2])),Integer.parseInt(Integer.toBinaryString(networkIpArrInt[3])));
+			networkIpBinaryFull = String.format("%08d.%08d.%08d.%08d", Integer.parseInt(Integer.toBinaryString(networkIpArrInt[0])),Integer.parseInt(Integer.toBinaryString(networkIpArrInt[1])),Integer.parseInt(Integer.toBinaryString(networkIpArrInt[2])),Integer.parseInt(Integer.toBinaryString(networkIpArrInt[3])));
 			
 			broadcastIp = subnetUtils.getInfo().getBroadcastAddress();
 			broadcastIpArr = networkIp.split("[.]");
 			int[] broadcastIpArrInt = { Integer.parseInt(broadcastIpArr[0]),Integer.parseInt(broadcastIpArr[1]) ,Integer.parseInt(broadcastIpArr[2]) ,Integer.parseInt(broadcastIpArr[3])};
-			broadcastIpBinary = String.format("%08d.%08d.%08d.%08d", Integer.parseInt(Integer.toBinaryString(broadcastIpArrInt[0])), Integer.parseInt(Integer.toBinaryString(broadcastIpArrInt[1])),Integer.parseInt(Integer.toBinaryString(broadcastIpArrInt[2])),Integer.parseInt(Integer.toBinaryString(broadcastIpArrInt[3])));
+			broadcastIpBinaryFull = String.format("%08d.%08d.%08d.%08d", Integer.parseInt(Integer.toBinaryString(broadcastIpArrInt[0])), Integer.parseInt(Integer.toBinaryString(broadcastIpArrInt[1])),Integer.parseInt(Integer.toBinaryString(broadcastIpArrInt[2])),Integer.parseInt(Integer.toBinaryString(broadcastIpArrInt[3])));
+		
+//----------------3.print subnet network, broadcast--------------------------------------------------------------------------------------------------//		
+			System.out.printf("Subnet (Network):%13s/%d %40s (Class %c)\n", networkIp , subnetPrefix , networkIpBinaryFull , ipClass);					 //
+			System.out.printf("Broadcast: %22s %40s\n", broadcastIp , broadcastIpBinaryFull);															 //
+//---------------------------------------------------------------------------------------------------------------------------------------------------//		
 			
-			System.out.printf("Subnet (Network):%13s/%d %40s (Class %c)\n", networkIp , subnetPrefix , SubnetMask.subnetMaskFull , ipClass);
-			System.out.printf("Broadcast: %22s %40s\n", broadcastIp , broadcastIpBinary);
 			
+			
+			
+			
+			/**
+			 * calculate host min, max, subnet bits, subnet total
+			 */
 			hostMinIp = subnetUtils.getInfo().getLowAddress();
 			hostMinIpArr = hostMinIp.split("[.]");
 			int[] hostMinIpArrInt = { Integer.parseInt(hostMinIpArr[0]),Integer.parseInt(hostMinIpArr[1]),Integer.parseInt(hostMinIpArr[2]),Integer.parseInt(hostMinIpArr[3])};
-			hostMinBinary = String.format("%08d.%08d.%08d.%08d", Integer.parseInt(Integer.toBinaryString(hostMinIpArrInt[0])),Integer.parseInt(Integer.toBinaryString(hostMinIpArrInt[1])),Integer.parseInt(Integer.toBinaryString(hostMinIpArrInt[2])),Integer.parseInt(Integer.toBinaryString(hostMinIpArrInt[3])));
+			hostMinBinaryFull = String.format("%08d.%08d.%08d.%08d", Integer.parseInt(Integer.toBinaryString(hostMinIpArrInt[0])),Integer.parseInt(Integer.toBinaryString(hostMinIpArrInt[1])),Integer.parseInt(Integer.toBinaryString(hostMinIpArrInt[2])),Integer.parseInt(Integer.toBinaryString(hostMinIpArrInt[3])));
 			
 			hostMaxIp = subnetUtils.getInfo().getHighAddress();
 			hostMaxIpArr = hostMaxIp.split("[.]");
 			int[] hostMaxIpArrInt = { Integer.parseInt(hostMaxIpArr[0]),Integer.parseInt(hostMaxIpArr[1]),Integer.parseInt(hostMaxIpArr[2]),Integer.parseInt(hostMaxIpArr[3])};
-			hostMaxBinary = String.format("%08d.%08d.%08d.%08d", Integer.parseInt(Integer.toBinaryString(hostMaxIpArrInt[0])),Integer.parseInt(Integer.toBinaryString(hostMaxIpArrInt[1])),Integer.parseInt(Integer.toBinaryString(hostMaxIpArrInt[2])),Integer.parseInt(Integer.toBinaryString(hostMaxIpArrInt[3])));
+			hostMaxBinaryFull = String.format("%08d.%08d.%08d.%08d", Integer.parseInt(Integer.toBinaryString(hostMaxIpArrInt[0])),Integer.parseInt(Integer.toBinaryString(hostMaxIpArrInt[1])),Integer.parseInt(Integer.toBinaryString(hostMaxIpArrInt[2])),Integer.parseInt(Integer.toBinaryString(hostMaxIpArrInt[3])));
+		
+//----------------------4.host minimum ip and maximum ip,subnet bits, subnet total-------------------------------------------------------------------//	
+			System.out.printf("HostMin (FHIP): %17s %40s\n", hostMinIp , hostMinBinaryFull);															 //
+			System.out.printf("HostMax (LHIP): %17s %40s\n", hostMaxIp , hostMaxBinaryFull);															 //		
+			System.out.printf("s=%d\n", s); //subnet bits																							 //
+			System.out.printf("S=%.0f\n", Math.pow(2,s)); //subnet total number																		 //	
+//---------------------------------------------------------------------------------------------------------------------------------------------------//
+		
 			
-			System.out.printf("HostMin (FHIP): %17s %40s\n", hostMinIp , hostMinBinary);
-			System.out.printf("HostMax (LHIP): %17s %40s\n", hostMaxIp , hostMaxBinary);
 			
-			System.out.printf("s=%d\n", s); //subnet bits
-			System.out.printf("S=%.0f\n", Math.pow(2,s)); //subnet total number
+		
 			
+			/**
+			 * calculate subnet index
+			 */
 			subnetIndex = s + 32;
 			subnetIndexBinary = Integer.parseInt(Integer.toBinaryString(s+32));//subnet index to binary
 			
-			System.out.printf("Subnet Index (%08d) = %d\n", subnetIndexBinary , subnetIndex);
-			System.out.printf("h= %d\n", h);
-			System.out.printf("HIPs Hosts/Net: %.0f\n", Math.pow(2, h));
+//----------------------5.subnet index, host bits, host total----------------------------------------------------------------------------------------//		
+			System.out.printf("Subnet Index (%08d) = %d\n", subnetIndexBinary , subnetIndex);														 //
+			System.out.printf("h= %d\n", h);																										 //
+			System.out.printf("HIPs Hosts/Net: %.0f\n", Math.pow(2, h));																			 //
+//---------------------------------------------------------------------------------------------------------------------------------------------------//	
 			
 			sc.close();
 		} catch (Exception e) {
--- Network/CST8208_21S_BA2/src/CST8208_21S_BA2/SubnetMask.java
+++ Network/CST8208_21S_BA2/src/CST8208_21S_BA2/SubnetMask.java
@@ -1,84 +1,79 @@
 package CST8208_21S_BA2;
-
-import java.math.BigInteger;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
+/**
+ * 
+ * {@summary SubnetMask class: calculating subnet mask ip, subnet mask binary, wildcard ip, wildcard binary. Any of variables with 'Full' is the result for printing} 
+ *
+ */
 public class SubnetMask {
-	StringBuilder a = new StringBuilder(); //for subnet mask binary
-	String b; //String type subnet mask binary
-	StringBuilder c = new StringBuilder(); //for wildcard binary
-	String d; //String type wildcard binary
-	double e; //Double type subnet binary
-	String subnetBinary;
-	static String subnetMaskFull; //subnet binary full (with dots)
-	static String subnetDecimalString; 
-	static String wildcardStr;
-	static byte[] subnetBinaryArr; //for subnet (array)
-	String[] subnetSepArr = new String[4]; //for subnetmask calculate (binary to decimal)
-	String[] subnetSepString = new String[4]; //string type - seperated
-	int[] subnetSepDArr = new int[4]; //subnet separated decimal array
+	StringBuilder subnetStrBuilder = new StringBuilder(); //for subnet mask binary (StringBuilder)
+	String subnetBinaryStr; //String type subnet mask binary (String)
+	double subnetBinaryDouble; //Double type subnet binary (double)
+	static String subnetMaskBinaryFull; //subnet binary full (with dots) (String)
 	
+	String[] subnetIpArr = new String[4]; //for subnetmask ip calculate (binary to decimal)
+	String[] subnetIpArrStr = new String[4]; //subnet mask ip seperated (String[])
+	int[] subnetIpArrInt = new int[4]; //subnet ip array (int[])
+	static String subnetIpString; //subnet ip full (with dots) (String)
+	
+	int[] wildcardArr = new int[4]; // wildcard ip (int[])
+	StringBuilder wildcardBinStrBuilder = new StringBuilder(); //for wildcard binary (StringBuilder)
+	String wildcardBinStrFull; //String type wildcard binary
+	static String wildcardStrFull; //wildcard string (String)
+	/**
+	 * 
+	 * @param subnetPrefix
+	 * @return subnetBinaryFull
+	 */
 	public String subnetMaskBinary(int subnetPrefix) { //binary from subnet prefix
 		
-//		try {
-//			subnetBinaryArr = InetAddress.getByName(ip).getAddress();
-//			subnetBinary = new BigInteger(1, subnetBinaryArr).toString(2); //calculate subnet to binary (String type)
-//			e = Double.parseDouble(subnetBinary);
-//		} catch (UnknownHostException e) {
-//			e.printStackTrace();
-//		}
-		
 		for(int i=0;i<subnetPrefix;i++) {
-			a = a.append('1');	
+			subnetStrBuilder = subnetStrBuilder.append('1');	
 		}
 		for(int j=subnetPrefix;j<32;j++) {
-			a = a.append('0');
+			subnetStrBuilder = subnetStrBuilder.append('0');
 		}
-		b = a.toString(); //subnet mask in binary (String type)
-		subnetMaskFull = b.substring(0,8) + "." + b.substring(8,16) + "." + b.substring(16,24) + "." + b.substring(24,32);
 		
-		subnetDecimalCalculate(subnetPrefix);
+		subnetBinaryStr = subnetStrBuilder.toString(); //subnet mask in binary (String type)
+		subnetMaskBinaryFull = subnetBinaryStr.substring(0,8) + "." + subnetBinaryStr.substring(8,16) + "." + subnetBinaryStr.substring(16,24) + "." + subnetBinaryStr.substring(24,32); //subnet mask binary full (String)
 		
-		return b;
+		subnetIpCalculate(subnetPrefix);
+		
+		return subnetMaskBinaryFull;
 	}
 	
-	public String subnetDecimalCalculate(int subnetPrefix) { //calculating subnet mask in ip format
-		subnetSepArr[0] = b.substring(0,8); //string type (String)
-		subnetSepArr[1]	= b.substring(8,16);
-		subnetSepArr[2] = b.substring(16,24);
-		subnetSepArr[3] = b.substring(24,32);			
+	public String subnetIpCalculate(int subnetPrefix) { //calculating subnet mask in IP format
+		subnetIpArr[0] = subnetBinaryStr.substring(0,8); //string type (String)
+		subnetIpArr[1]	= subnetBinaryStr.substring(8,16);
+		subnetIpArr[2] = subnetBinaryStr.substring(16,24);
+		subnetIpArr[3] = subnetBinaryStr.substring(24,32);			
 		
 		int intBeforeStr;
-		subnetSepString = new String[4];
 
 		for(int i=0;i<4;i++) {
-			intBeforeStr = Integer.valueOf(subnetSepArr[i], 2);
-			subnetSepString[i] = String.valueOf(intBeforeStr); //to String type
+			intBeforeStr = Integer.valueOf(subnetIpArr[i], 2);
+			subnetIpArrStr[i] = String.valueOf(intBeforeStr); //to String type
 		}
-		subnetMaskFull = subnetSepArr[0] + "." + subnetSepArr[1] + "."  + subnetSepArr[2] + "." + subnetSepArr[3]; //binary subnetmask full
-		subnetDecimalString = subnetSepString[0] + "." + subnetSepString[1] + "." + subnetSepString[2] + "." + subnetSepString[3]; //subnetmask ip full
+		subnetIpString = subnetIpArrStr[0] + "." + subnetIpArrStr[1] + "." + subnetIpArrStr[2] + "." + subnetIpArrStr[3]; //subnetmask ip full with dots (String)
 		wildcardCalculate(subnetPrefix);
-		return subnetDecimalString;
+		return subnetIpString;
 	} //subnetDecimalCalculate end
 	
 	public String wildcardCalculate(int subnetPrefix) {
-		int[] wildcardArr = new int[4];
-
-		for(int i=0;i<4;i++) {
-			wildcardArr[i] = 255 - Integer.parseInt(subnetSepString[i]);
+		for(int i=0;i<4;i++) { //calculate wildcard ip (int[])
+			wildcardArr[i] = 255 - Integer.parseInt(subnetIpArrStr[i]);
 		}
-		wildcardStr = String.valueOf(wildcardArr[0]) + "." + String.valueOf(wildcardArr[1]) + "." + String.valueOf(wildcardArr[2]) + "." +String.valueOf(wildcardArr[3]); //in ipformats with dots
+		wildcardStrFull = String.valueOf(wildcardArr[0]) + "." + String.valueOf(wildcardArr[1]) + "." + String.valueOf(wildcardArr[2]) + "." +String.valueOf(wildcardArr[3]); //in ipformats with dots (String)
+		
 		for(int x=0;x<subnetPrefix;x++) {
-			c = c.append('0');
+			wildcardBinStrBuilder = wildcardBinStrBuilder.append('0');
 		}
 		for(int y=subnetPrefix;y<32;y++) {
-			c = c.append('1');
+			wildcardBinStrBuilder = wildcardBinStrBuilder.append('1');
 		}
+		wildcardBinStrFull=wildcardBinStrBuilder.toString();
+		wildcardBinStrFull=wildcardBinStrFull.substring(0,8) + "." + wildcardBinStrFull.substring(8,16) + "." + wildcardBinStrFull.substring(16,24) + "." + wildcardBinStrFull.substring(24,32); //wildcard binary full with dots (String)
 		
-		d=c.toString();
-		d=d.substring(0,8) + "." + d.substring(8,16) + "." + d.substring(16,24) + "." + d.substring(24,32); //wildcard binary full
-		return d;
+		return wildcardBinStrFull;
 	} //wildcardCalculate end
 	
 }
