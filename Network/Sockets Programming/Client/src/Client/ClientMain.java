9ae0f7bf107d5f76fa54d3d9121a3fd857cff720
dinggididudara <dinggididududu@gmail.com>
2021-08-11 19:36:03 -0400

    Linux script and network programming

--- Network/Sockets Programming/Client/src/Client/ClientMain.java
+++ Network/Sockets Programming/Client/src/Client/ClientMain.java
@@ -3,6 +3,6 @@ package Client;
 public class ClientMain {
 	public static void main(String[] args) {
 		SimpleClient client = new SimpleClient(); //new client
-		client.start();
+		client.connect();
 	}
 }
